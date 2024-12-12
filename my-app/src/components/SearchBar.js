8006f6f670f9b615db31e886d6ac2196e703f0e2
SheharyarXDツ <1510sheharyarxd@gmail.com>
2024-12-13 00:49:46 +0500

    Logi Sign up

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -8,12 +8,12 @@ import { useMap } from './MapView.js';
 const SearchBar = () => {
   const recentLocations = ['New York', 'Los Angeles', 'Chicago', 'San Francisco', 'Miami'];
   const { locations, fetchBookMarkedLocations, addBookMarkedLocation, deleteBookMarkedLocation, loading, error } = useLocationsContext();
-  const { login } = useContext(AuthContext);
+  const { login,user } = useContext(AuthContext);
   const { addLocation, fetchLocations, searchedLocations, deleteLocation } = useContext(SearchedLocationsContext);
   const [isFocused, setIsFocused] = useState(false);
   const [sourceLocation, setSourceLocation] = useState('');
   const [destinationLocation, setDestinationLocation] = useState('');
-  const [userID] = useState(1); // Replace with dynamic user ID if available
+  const userID = user?.userId;
   const [searchQuery, setSearchQuery] = useState('');
   const [locationSelected, setLocationSelected] = useState(null);
   const {l,setL}=useContext(SearchedLocationsContext);
